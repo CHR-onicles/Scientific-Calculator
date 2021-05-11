2b6f223f4957588be07e8cd4a8a543f6827444af
CHR-onicles <tpandivine48@gmail.com>
2021-05-11 13:52:46 +0000

    added initial stuff

--- src/UI_standard_calculator.py
+++ src/UI_standard_calculator.py
@@ -2,3 +2,21 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('Standard Calculator')
+        self.setFixedSize(500, 700)
+
+        self.widgets()
+        self.layouts()
+
+    def widgets(self):
+        pass
+
+    def layouts(self):
+        pass
+
---
+++ src/main.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+
+from UI_standard_calculator import MainWindow
+
+
+
+
+class MainApp(MainWindow):
+
+    def __init__(self):
+        super(MainApp, self).__init__()
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    sys.exit(app.exec())
