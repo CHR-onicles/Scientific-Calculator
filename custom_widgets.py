edfa67d9ac0992975e254560f30fb9bf9240fe14
CHR-onicles <tpandivine48@gmail.com>
2021-05-18 13:01:16 +0000

    Update custom_widgets.py

--- custom_widgets.py
+++ custom_widgets.py
@@ -4,22 +4,26 @@ from PyQt5.QtCore import *
 
 
 class PushButton(QPushButton):
+    BASE_COLOR = '#202020'
+    HOVER_COLOR = '#505050'
+    PRESS_COLOR = '#707070'
 
     def __init__(self, parent=None):
         super(PushButton, self).__init__(parent)
         self._animation1 = QVariantAnimation(
-            startValue=QColor('#505050'),
-            endValue=QColor('#202020'),
+            startValue=QColor(self.HOVER_COLOR),
+            endValue=QColor(self.BASE_COLOR),
             valueChanged=self._on_value_changed,
             duration=400,
         )
         self._animation2 = QVariantAnimation(
-            startValue=QColor('#707070'),
-            endValue=QColor('#505050'),
-
+            startValue=QColor(self.PRESS_COLOR),
+            endValue=QColor(self.HOVER_COLOR),
+            valueChanged=self._on_value_changed,
+            duration=200,
         )
-        
-        self._update_stylesheet(QColor('#202020'))
+
+        self._update_stylesheet(QColor(self.BASE_COLOR))  # passing in default background color
         self.setCursor(Qt.PointingHandCursor)
 
     def _on_value_changed(self, color):
@@ -46,6 +50,13 @@ class PushButton(QPushButton):
         self._animation1.start()
         super().leaveEvent(event)
         
-    def mousePressEvent(self, *args, **kwargs):
-        
+    def mousePressEvent(self, event, *args, **kwargs):
+        self._animation2.setDirection(QAbstractAnimation.Backward)
+        self._animation2.start()
+        super(PushButton, self).mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event, *args, **kwargs):
+        self._animation2.setDirection(QAbstractAnimation.Forward)
+        self._animation2.start()
+        super(PushButton, self).mouseReleaseEvent(event)
 
